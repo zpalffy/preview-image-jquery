099334e2fd5b43107a4fee125e85b1fe3479a49a
zpalffy <zpalffy@gmail.com>
2013-03-20 00:42:03 -0500

    Update preview-image-1.0.js

--- preview-image-1.0.js
+++ preview-image-1.0.js
@@ -1,39 +1,11 @@
 /**
  * preview-image-1.0.js
  * Eric Olson
- *
- * Much of the code was taken from Alen Grakalic (http://cssglobe.com)  I took his already-working
- * code, and modified it to:
- *  - use a 'data-' element rather than requiring a wrapping anchor tag
- *  - include the css for the overlay in script.
- *  - add the options map for easily changing how it works.
- *  - change to use jQuery's 'on' methods so that dynamically added elements also acquire the 
- *    preview behavior.  Also, we don't need to wait for document.ready to 'bind,' therefore no
- *    method call is required to configure the plugin (unless non-default config values should
- *    be used.)
- *
- * Original: http://cssglobe.com/easiest-tooltip-and-image-preview-using-jquery/
- *
- * To use, simply include the script in the head of your page.  If you add an attribute to any
- * element like:
- * <img src="thumbnail-view.jpg" data-preview-image="large-view.jpg" />
- * then when a user hovers the mouse over the image, the larger view will be displayed.
- *
- * Another example:
- * <a href="http://somesite.com/somepage.html" data-preview-image="preview-of-page.jpg"
- *
- * If you don't like the defaults, you can optionally change any or all of them like:
- * $.previewImage(
- *   'xOffset': 10,  // x-offset from cursor
- *   'yOffset': 10,  // y-offset from cursor
- *   'fadeIn': 1000, // delay in ms. to display the preview
- *   'css': {        // the following css will be used when rendering the preview image.
- *      'padding': '20px',
- *      'border': 5px solid black'
- *   }
- * );
+ * https://github.com/zpalffy/preview-image-jquery
+ * 
+ * Original: http://cssglobe.com/easiest-tooltip-and-image-preview-using-jquery/ by Alen Grakalic (http://cssglobe.com)
  */
-(function($) {
+ (function($) {
 	$.previewImage = function(options) {		
 		var opts = $.extend({
 			/* The following set of options are the ones that should most often be changed by
